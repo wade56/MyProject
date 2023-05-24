a3b7fa3f02afb45ba408bd14f919068fff79054c
Wade <wade@avalonengineer.com>
2023-05-23 21:25:44 -0700

    Add project files.

---
+++ MyUserControl.xaml.vb
@@ -0,0 +1,17 @@
+﻿Public Class MyUserControl
+
+   Inherits UserControl
+
+   Public Shared ReadOnly TextProperty As DependencyProperty =
+        DependencyProperty.Register("Text", GetType(String), GetType(MyUserControl))
+
+   Public Property Text As String
+      Get
+         Return CStr(GetValue(TextProperty))
+      End Get
+      Set(value As String)
+         SetValue(TextProperty, value)
+      End Set
+   End Property
+
+End Class
